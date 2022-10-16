7dbdeb4c0668feca5798f2e4fe38765c10eec331
gift56 <efeasiughu@gmail.com>
2022-10-16 17:30:09 +0100

    imported icons

--- src/components/aboutMe/About.jsx
+++ src/components/aboutMe/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Facebook from "../../assets/Facebook.png";
+import Instagram from "../../assets/instagram.png";
+import Linkedin from "../../assets/linkedin.png";
+import Github from "../../assets/github.png";
+import Behance from "../../assets/behance.png";
+import Dribble from "../../assets/dribble.png";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const aboutLinks = [
+    {
+      img: Facebook,
+      link: "Facebook",
+    },
+    {
+      img: Instagram,
+      link: "Instagram",
+    },
+    {
+      img: Linkedin,
+      link: "Linkedlin",
+    },
+    {
+      img: Github,
+      link: "Github",
+    },
+    {
+      img: Behance,
+      link: "Behance",
+    },
+    {
+      img: Dribble,
+      link: "Dribble",
+    },
+  ];
+  return <div>About</div>;
+};
 
-export default About
+export default About;
