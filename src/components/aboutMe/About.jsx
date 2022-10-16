384bf93d1ab4a3219bb2ae8fb9971368b25efef1
gift56 <efeasiughu@gmail.com>
2022-10-16 17:41:19 +0100

    moving tostylings

--- src/components/aboutMe/About.jsx
+++ src/components/aboutMe/About.jsx
@@ -37,7 +37,35 @@ const About = () => {
   return (
     <section id="About" className={`${AboutStyle.aboutCon}`}>
       <div className="container">
-        <div></div>
+        <div className={`${AboutStyle.aboutArea}`}>
+          <h2>About Me</h2>
+          <p>
+            I’m a goal-focused individual with 4 years of experience in UI/UX
+            design. I’m passionate about creating a user-centered design,
+            gathering and translating user data into design decisions and
+            products. My current position as a Product Designer has allowed me
+            to hone a key design eye while utilizing top-notch technical
+            ability. I can design user-interface smooth pages that promote user
+            interaction with the information and in addition to good visual
+            abilities, I also possess great presentation skills that allow me to
+            successfully present design ideas to stakeholders. I am an
+            enthusiastic and creative product designer with experience, skilled
+            at turning the objective of the product into a functional user
+            experience. A seasoned leader and program manager with outstanding
+            communication and problem-solving skills. Able to make firm
+            decisions in favor of the client.
+          </p>
+          <div className={`${AboutStyle.Links}`}>
+            {aboutLinks.map(({ img, link }, index) => (
+              <div key={index} className={`${AboutStyle.Link}`}>
+                <a href={`${link}`}>
+                  <img src={img} alt={link} />
+                  <p>{link}</p>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
