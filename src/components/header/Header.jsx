9b947a73cda97acba8fea350ac1c56ad20a6564c
gift56 <efeasiughu@gmail.com>
2022-10-16 16:52:34 +0100

    added font family

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,17 +9,21 @@ const Header = () => {
     <div className={`${HeaderStyle.headerCon}`}>
       <div className="container">
         <div className={`${HeaderStyle.headerArea}`}>
-            <div className={`${HeaderStyle.logo}`}>
-              <img src={Logo} alt="logo" />
-            </div>
-            <ul className={`${HeaderStyle.links}`}>
-              {links.map((link) => (
-                <li key={link}>
-                  <a href={`#${link}`}>{link}</a>
-                </li>
-              ))}
-            </ul>
-            <Button />
+          <div className={`${HeaderStyle.logo}`}>
+            <img src={Logo} alt="logo" />
+          </div>
+          <ul className={`${HeaderStyle.links}`}>
+            {links.map((link) => (
+              <li key={link}>
+                <a href={`#${link}`}>{link}</a>
+              </li>
+            ))}
+          </ul>
+          <Button
+            className={`${HeaderStyle.button}`}
+            text="Contact Me"
+            link="contactme"
+          />
         </div>
       </div>
     </div>
