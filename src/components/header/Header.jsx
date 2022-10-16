cedc218c2ff781a9ce89cfd573b16856195bb983
gift56 <efeasiughu@gmail.com>
2022-10-16 16:37:12 +0100

    added some stuff

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
 import HeaderStyle from "./header.module.css";
 
 const Header = () => {
-  return <div className={`${HeaderStyle.bg}`}>Header</div>;
+  const links = ["About", "Skills", "Portfolio"];
+  return (
+    <div className={`${HeaderStyle.headerCon}`}>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <ul></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
