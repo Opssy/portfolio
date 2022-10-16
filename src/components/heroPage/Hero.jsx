47c9d9f82bd183241eacaea722416b8498019316
gift56 <efeasiughu@gmail.com>
2022-10-16 17:12:31 +0100

    imported heroimage

--- src/components/heroPage/Hero.jsx
+++ src/components/heroPage/Hero.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import HeroImg from "../../assets/heroimg.png";
+import HappyImg from "../../assets/happy.png";
 import HeroStyle from "./hero.module.css";
+import Button from "../buttonComponents/Button";
 
 const Hero = () => {
   return (
     <div className={`${HeroStyle.heroCon}`}>
       <div className="container">
         <div className={`${HeroStyle.heroArea}`}>
-          <div className={`${HeroStyle.heroLeft}`}></div>
+          <div className={`${HeroStyle.heroLeft}`}>
+            <h4>
+              Hello there <img src={HappyImg} alt="smiling_emoji" />
+            </h4>
+            <h1>Hi, I am Temitope Williams (TEMS)</h1>
+            <p>
+              I am a product designer, Ux researcher and a front-end developer
+            </p>
+            <div className={`${HeroStyle.buttons}`}>
+              <Button
+                className={`${HeroStyle.firstbtn}`}
+                text="Read More"
+                link="aboutme"
+              />
+              <Button
+                className={`${HeroStyle.secondbtn}`}
+                text="Download Resume"
+                link="/resume"
+              />
+            </div>
+          </div>
           <div className={`${HeroStyle.heroRight}`}>
             <img src={HeroImg} alt="hero_image" />
           </div>
