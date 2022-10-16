ca17056305a7805009f98aa196f71dd0db7b97c2
gift56 <efeasiughu@gmail.com>
2022-10-16 17:55:43 +0100

    importing some stuff

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import Figma from "../../assets/figma.png";
+import Adobe from "../../assets/adobe.png";
+import Adobe2 from "../../assets/adobe2.png";
+import Adobe3 from "../../assets/adobe3.png";
+import Adobe4 from "../../assets/adobe4.png";
+import Adobe5 from "../../assets/adobe5.png";
+import Adobe6 from "../../assets/adobe6.png";
+import Invision from "../../assets/invision.png";
+import Invision2 from "../../assets/invision2.png";
+import Framer from "../../assets/framer.png";
+import Marvel from "../../assets/marvel.png";
+import Zeplin from "../../assets/zeplin.png";
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  return <div>Skills</div>;
+};
 
-export default Skills
+export default Skills;
