fd66a68eabf1a81665a6132414fa32c1c30d1e7c
gift56 <efeasiughu@gmail.com>
2022-10-16 17:05:59 +0100

    imported heroimag

--- src/components/heroPage/Hero.jsx
+++ src/components/heroPage/Hero.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import HeroImg from "../../assets/heroimg.png";
-import HeroStyle from './hero.module.css';
+import HeroStyle from "./hero.module.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={`${HeroStyle.heroCon}`}>
+      <div className="container">
+        <div className={`${HeroStyle.heroArea}`}>
+          <div className={`${HeroStyle.heroLeft}`}></div>
+          <div className={`${HeroStyle.heroRight}`}>
+            <img src={HeroImg} alt="hero_image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
